02a2b4e987c409377d2ada85845288b13e5e3c74
Rafli Mahadika <73173502+RafliMahadika@users.noreply.github.com>
2024-12-27 21:19:06 +0700

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,17 @@
+use Ujian;
+CREATE TABLE tabel_mata_kuliahh (ID int NOT NULL, Mata_Kuliah char(50) NOT NULL, Nim int NOT NULL, Nilai int NOT NULL, 
+Dosen_Pengajar char(50) NOT NULL
+);
+INSERT INTO tabel_mata_kuliahh (ID, Mata_Kuliah, Nim, Nilai, Dosen_Pengajar) VALUES
+('1', 'Pemograman Web', '123456', '85', 'Pak Budi'),
+('2', 'Basis Data', '234567', '70', 'Ibu Ani'),
+('3', 'Jaringan Komputer', '345678', '75', 'Pak Dodi'),
+('4', 'Sistem Operasi', '123456','90', 'Pak Budi'),
+('5', 'Manajemen Proyek', '4567890', '80', 'Ibu Desi'),
+('6', 'Bahasa Inggris', '567890', '85', 'Ibu Eka'),
+('7', 'Statistika', '678901', '75', 'Pak Farhan'),
+('8', 'Algoritma', '789012', '65' , 'Pak Galih'),
+('9', 'Pemograman Java', '123456', '95', 'Pak Budi');
+
+
+select *from  tabel_mata_kuliahh
---
+++ SQLQuery3.sql
@@ -0,0 +1,30 @@
+use Ujian;
+CREATE TABLE tabel_mahasiswaa (NIM char(30) DEFAULT NULL,Nama char(50) NOT NULL,
+Alamat char(250) NOT NULL,
+  Jurusan char(50) NOT NULL,Umur char(30) NOT NULL
+)
+
+
+INSERT INTO tabel_mahasiswaa (NIM, Nama, Alamat, Jurusan, Umur) VALUES
+('123456', 'John', 'Jl Merdeka No 1', 'Teknik Informatika', '21'),
+('234567', 'Alice', 'Jl Gatot Subroto', 'Sistem Informasi', '23'),
+('345678', 'Bob', 'Jl Sudirman No 5', 'Teknik Informatika', '20'),
+('456789', 'Cindy', 'Jl Pahlawan No 2', 'Manajemen', '22'),
+('567890', 'David', 'Jl Dipnegoro No 3', 'Teknik Elektro', '25'),
+('678901', 'Emily', 'Jl Cendrawasih No 4', 'Manajemen', '24'),
+('789012', 'Frank', 'Jl Ahmad Yani No 6', 'Teknik Informatika', '19');
+
+
+update tabel_mahasiswaa set Nama = 'John', Alamat = 'Jl Raya No 5' where NIM = '123456';
+
+select *from tabel_mahasiswaa
+
+select Nama,Jurusan,Dosen_Pengajar  from tabel_mahasiswaa,tabel_mata_kuliahh  where Jurusan = 'Teknik Informatika' 
+
+select Nama from tabel_mahasiswaa order by Umur Desc
+
+select tabel_mahasiswaa.NIM, tabel_mahasiswaa.Nama,tabel_mahasiswaa.Jurusan, tabel_mata_kuliahh.Dosen_Pengajar from tabel_mahasiswaa 
+cross join tabel_mata_kuliahh where Jurusan = 'Teknik Informatika' and tabel_mata_kuliahh.Nim = '123456' 
+
+select Nama, Mata_Kuliah ,Nilai from tabel_mahasiswaa cross join tabel_mata_kuliahh where Nilai > '70'
+
